533ee8f68a080080357fb007ec0149ddeedca796
James <admin@floretos.com>
2025-05-11 19:17:23 -0400

    fix: remove need to be signed in to get token

--- internal/resolvers/refresh_token.go
+++ internal/resolvers/refresh_token.go
@@ -4,18 +4,12 @@ import (
 	"context"
 	"fmt"
 	"github.com/weeb-vip/auth/graph/model"
-	"github.com/weeb-vip/auth/http/handlers/requestinfo"
 	"github.com/weeb-vip/auth/internal/jwt"
 	"github.com/weeb-vip/auth/internal/services/refresh_token"
 	"github.com/weeb-vip/auth/internal/services/session"
 )
 
 func RefreshToken(ctx context.Context, sessionService session.Session, refreshTokenService refresh_token.RefreshToken, jwtTokenizer jwt.Tokenizer, token string) (*model.SigninResult, error) {
-	req := requestinfo.FromContext(ctx)
-	userid := req.UserID
-	if userid == nil {
-		return nil, fmt.Errorf("user id is missing")
-	}
 
 	refreshToken, err := refreshTokenService.GetToken(token)
 	if err != nil {
@@ -23,10 +17,6 @@ func RefreshToken(ctx context.Context, sessionService session.Session, refreshTo
 	}
 	if refreshToken == nil {
 		return nil, fmt.Errorf("refresh token not found")
-	}
-
-	if refreshToken.UserID != *userid {
-		return nil, fmt.Errorf("refresh token not valid")
 	}
 
 	session, err := sessionService.CreateSession(ctx, refreshToken.UserID)
